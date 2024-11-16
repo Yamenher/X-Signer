35d5c41bf079c08be638b279974dad672c9a42b6
Yamenher <hermassiyamen@gmail.com>
2024-11-16 07:29:07 +0100

    Feat : started working on new about screen

---
+++ README.md
@@ -0,0 +1,57 @@
+X-Signer
+
+X-Signer is a lightweight Android app designed for signing APK/AAB files with various signature schemes (v1, v2, v3, v4). It provides an easy-to-use interface to help users securely sign their apps without hassle.
+
+Features
+
+Supports signing APKs and AABs with multiple signature versions (v1, v2, v3, v4).
+
+Option to enable both v1 + v2 signatures or all signature versions (v1, v2, v3, v4).
+
+Local storage for device data without cloud integration.
+
+Encrypts all locally stored data for security.
+
+Device information is collected (e.g., model, Android version, brand), but no personal data is ever collected.
+
+Uses Google Firebase solely for update checks.
+
+
+How to Use
+
+1. Install the App
+Download and install the APK on your Android device.
+
+
+2. Select the APK/AAB file
+Choose the APK or AAB file you want to sign.
+
+
+3. Select Signature Versions
+Choose the desired signature versions (v1, v2, v3, v4) from the app's settings.
+
+
+4. Sign the APK/AAB
+Tap the 'Sign' button to sign your file. The signed file will be saved locally.
+
+
+
+Permissions
+
+Storage Access: Required for reading and saving APK/AAB files.
+
+Internet Access: Used only for checking app updates via Google Firebase.
+
+
+Privacy
+
+The app does not collect or share any personal data.
+
+All data is stored locally and encrypted for security.
+
+The app does not target children.
+
+
+License
+
+This project is licensed under the MIT License - see the LICENSE file for details.
--- app/src/main/java/com/xapps/utility/xsigner/InfoActivity.java
+++ app/src/main/java/com/xapps/utility/xsigner/InfoActivity.java
@@ -3,113 +3,43 @@ package com.xapps.utility.xsigner;
 import android.animation.ArgbEvaluator;
 import android.content.Intent;
 import android.content.res.*;
+import android.view.*;
 import android.os.Build;
-import android.window.OnBackInvokedDispatcher;
-import androidx.activity.OnBackPressedCallback;
 import androidx.activity.OnBackPressedDispatcher;
 import android.graphics.*;
 import android.net.Uri;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
-import android.view.*;
-import android.view.View;
 import androidx.core.content.pm.PackageInfoCompat;
 import java.util.Random;
 import java.util.ArrayList;
 import android.util.DisplayMetrics;
 import android.widget.*;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 import androidx.activity.*;
 import android.util.TypedValue;
 import android.util.SparseBooleanArray;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.resources.*;
-import androidx.appcompat.widget.Toolbar;
-import androidx.coordinatorlayout.widget.CoordinatorLayout;
-import com.google.android.material.appbar.AppBarLayout;
-import com.google.android.material.appbar.CollapsingToolbarLayout;
-import com.mursaat.extendedtextview.*;
-import de.hdodenhof.circleimageview.*; 
 import java.io.InputStream;
-import androidx.core.widget.NestedScrollView;
-import com.google.android.material.transition.platform.MaterialSharedAxis;
+import com.google.android.material.transition.platform.*;
 import com.google.android.material.internal.EdgeToEdgeUtils;
-import com.google.android.material.transition.platform.MaterialContainerTransform;
-import com.google.android.material.transition.platform.MaterialArcMotion;
-import com.google.android.material.shape.ShapeAppearanceModel;
-import com.google.android.material.shape.CornerFamily;
-import com.google.android.material.transition.platform.MaterialContainerTransformSharedElementCallback;
+import com.google.android.material.shape.*;
 import androidx.core.app.ActivityOptionsCompat;
-import com.google.android.material.appbar.MaterialToolbar;
+import com.xapps.utility.xsigner.databinding.InfoBinding;
 
 
 
 public class InfoActivity extends AppCompatActivity {
 	
-	private MaterialToolbar _toolbar;
-	private AppBarLayout _app_bar;
-	private CoordinatorLayout _coordinator;
+    private InfoBinding binding;
 	private String versionName = "";
 	private String versionCode = "";
-	
-	private CollapsingToolbarLayout collapsingtoolbar;
-	private NestedScrollView Scroller;
-	private LinearLayout BG;
-	private LinearLayout VersionsLinear;
-	private LinearLayout XEngineVersionLinear;
-	private LinearLayout ThanksLinear;
-	private LinearLayout VersionLinear;
-	private LinearLayout Dv1;
-	private LinearLayout VersionCodeLinear;
-	private LinearLayout Dv2;
-	private LinearLayout ProductionPhaseLinear;
-	private LinearLayout UpdateLinear;
-	private TextView VersionNameText;
-	private TextView VersionNameDetails;
-	private TextView VersionCodeText;
-	private TextView VersionCodeDetails;
-	private TextView ProductioPhaseText;
-	private TextView ProductionPhaseDetails;
-	private TextView UpdateText;
-	private ImageView UpdateIcon;
-	private LinearLayout EngineNameLinear;
-	private LinearLayout Dv3;
-	private LinearLayout XEngineContainer;
-	private TextView PoweredByTitle;
-	private AnimatedGradientTextView XEngineLogoAnimated;
-	private TextView XEngineVersionTitle;
-	private TextView XEngineVersionDetail;
-	private TextView SpecialThanksTitle;
-	private LinearLayout User1;
-	private LinearLayout Dv4;
-	private LinearLayout User2;
-	private LinearLayout Dv5;
-	private LinearLayout User4;
-	private LinearLayout Dv6;
-	private LinearLayout User3;
-	private CircleImageView Pic1;
-	private LinearLayout NameAndWhy1;
-	private TextView Username1;
-	private TextView Role1;
-	private CircleImageView Pic2;
-	private LinearLayout NameAndWhy2;
-	private TextView Username2;
-	private TextView Role2;
-	private CircleImageView Pic4;
-	private LinearLayout NameAndWhy4;
-	private TextView Username4;
-	private TextView Role4;
-	private CircleImageView Pic3;
-	private LinearLayout NameAndWhy3;
-	private TextView Username3;
-	private TextView Role3;
 	
 	private Intent UserViewIntent = new Intent();
 	
     @Override
 	protected void onCreate(Bundle _savedInstanceState) {
+        binding = InfoBinding.inflate(getLayoutInflater());
         getWindow().setAllowEnterTransitionOverlap(true);
         MaterialSharedAxis enterTransition = new MaterialSharedAxis(MaterialSharedAxis.Y, true);
         enterTransition.addTarget(R.id._coordinator);
@@ -120,92 +50,26 @@ public class InfoActivity extends AppCompatActivity {
         returnTransition.addTarget(R.id._coordinator);
         getWindow().setReturnTransition(returnTransition);
 		super.onCreate(_savedInstanceState);
-		setContentView(R.layout.info);
+		setContentView(binding.getRoot());
 		initialize(_savedInstanceState);
 		initializeLogic();
     }
 	
 	private void initialize(Bundle _savedInstanceState) {
-		_app_bar = findViewById(R.id._app_bar);
-		_coordinator = findViewById(R.id._coordinator);
-		_toolbar = findViewById(R.id._toolbar);
-		setSupportActionBar(_toolbar);
+		
+		setSupportActionBar(binding.Toolbar);
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 		getSupportActionBar().setHomeButtonEnabled(true);
-		_toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+		binding.Toolbar.setNavigationOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View _v) {
 				getOnBackPressedDispatcher().onBackPressed();
-			}
-		});
-		collapsingtoolbar = findViewById(R.id.collapsingtoolbar);
-		Scroller = findViewById(R.id.Scroller);
-		BG = findViewById(R.id.BG);
-		VersionsLinear = findViewById(R.id.VersionsLinear);
-		XEngineVersionLinear = findViewById(R.id.XEngineVersionLinear);
-		ThanksLinear = findViewById(R.id.ThanksLinear);
-		VersionLinear = findViewById(R.id.VersionLinear);
-		Dv1 = findViewById(R.id.Dv1);
-		VersionCodeLinear = findViewById(R.id.VersionCodeLinear);
-		Dv2 = findViewById(R.id.Dv2);
-		ProductionPhaseLinear = findViewById(R.id.ProductionPhaseLinear);
-		UpdateLinear = findViewById(R.id.UpdateLinear);
-		VersionNameText = findViewById(R.id.VersionNameText);
-		VersionNameDetails = findViewById(R.id.VersionNameDetails);
-		VersionCodeText = findViewById(R.id.VersionCodeText);
-		VersionCodeDetails = findViewById(R.id.VersionCodeDetails);
-		ProductioPhaseText = findViewById(R.id.ProductioPhaseText);
-		ProductionPhaseDetails = findViewById(R.id.ProductionPhaseDetails);
-		UpdateText = findViewById(R.id.UpdateText);
-		UpdateIcon = findViewById(R.id.UpdateIcon);
-		EngineNameLinear = findViewById(R.id.EngineNameLinear);
-		Dv3 = findViewById(R.id.Dv3);
-		XEngineContainer = findViewById(R.id.XEngineContainer);
-		PoweredByTitle = findViewById(R.id.PoweredByTitle);
-		XEngineLogoAnimated = findViewById(R.id.XEngineLogoAnimated);
-		XEngineVersionTitle = findViewById(R.id.XEngineVersionTitle);
-		XEngineVersionDetail = findViewById(R.id.XEngineVersionDetail);
-		SpecialThanksTitle = findViewById(R.id.SpecialThanksTitle);
-		User1 = findViewById(R.id.User1);
-		Dv4 = findViewById(R.id.Dv4);
-		User2 = findViewById(R.id.User2);
-		Dv5 = findViewById(R.id.Dv5);
-		User4 = findViewById(R.id.User4);
-		Dv6 = findViewById(R.id.Dv6);
-		User3 = findViewById(R.id.User3);
-		Pic1 = findViewById(R.id.Pic1);
-		NameAndWhy1 = findViewById(R.id.NameAndWhy1);
-		Username1 = findViewById(R.id.Username1);
-		Role1 = findViewById(R.id.Role1);
-		Pic2 = findViewById(R.id.Pic2);
-		NameAndWhy2 = findViewById(R.id.NameAndWhy2);
-		Username2 = findViewById(R.id.Username2);
-		Role2 = findViewById(R.id.Role2);
-		Pic4 = findViewById(R.id.Pic4);
-		NameAndWhy4 = findViewById(R.id.NameAndWhy4);
-		Username4 = findViewById(R.id.Username4);
-		Role4 = findViewById(R.id.Role4);
-		Pic3 = findViewById(R.id.Pic3);
-		NameAndWhy3 = findViewById(R.id.NameAndWhy3);
-		Username3 = findViewById(R.id.Username3);
-		Role3 = findViewById(R.id.Role3);
-		
-		UpdateLinear.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View _view) {
-				if (false) {
-					_MakeTransition();
-				}
-				else {
-					XUtil.showMessage(getApplicationContext(), "It's a nightly bruh, don't expect everything in one update");
-				}
 			}
 		});
 	}
 	
 	private void initializeLogic() {
 		EdgeToEdgeUtils.applyEdgeToEdge(getWindow(), true);
-		collapsingtoolbar.setTitleEnabled(false);
 		try {
 		    android.content.pm.PackageInfo packageInfo = InfoActivity.this.getPackageManager().getPackageInfo(getPackageName(), 0);
 			versionName = packageInfo.versionName;
@@ -217,28 +81,10 @@ public class InfoActivity extends AppCompatActivity {
 		} catch (android.content.pm.PackageManager.NameNotFoundException e) {
 			e.printStackTrace();
 		}
-		if (Build.VERSION.SDK_INT >= 29) {
-            _toolbar.getNavigationIcon().setColorFilter(new BlendModeColorFilter(getColor(R.color.md_theme_textMain), BlendMode.SRC_ATOP));
-            UpdateIcon.setColorFilter(new BlendModeColorFilter(getColor(R.color.md_theme_textMain), BlendMode.SRC_ATOP));
-        } else {
-		    _toolbar.getNavigationIcon().setColorFilter(getColor(R.color.md_theme_textMain), PorterDuff.Mode.SRC_IN);
-	    	UpdateIcon.setColorFilter(getColor(R.color.md_theme_textMain), PorterDuff.Mode.SRC_IN);
-        }
-		
-		collapsingtoolbar.setTitle("About X-Signer");
-		Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/product_sans.ttf"); 
-		collapsingtoolbar.setCollapsedTitleTypeface(tf); 
-		collapsingtoolbar.setExpandedTitleTypeface(tf); 
-		int nightModeMask = getResources().getConfiguration().uiMode & android.content.res.Configuration.UI_MODE_NIGHT_MASK;
-		
-		if (nightModeMask == android.content.res.Configuration.UI_MODE_NIGHT_YES) {
-		
-					collapsingtoolbar.setCollapsedTitleTextColor(0xFFFFFFFF);
-			collapsingtoolbar.setExpandedTitleColor(0xFFFFFFFF);
-		} else {
-					collapsingtoolbar.setCollapsedTitleTextColor(0xFF000000);
-			collapsingtoolbar.setExpandedTitleColor(0xFF000000);
-		}
+        
+        binding.versionText.setText("v"+versionName+" ("+versionCode+")");
+		setTitle("About this app");
+        
 		int navigationBarHeight = 0;
 		int statusBarHeight= 0;
 		int r1 = getResources().getIdentifier("navigation_bar_height", "dimen", "android");
@@ -249,90 +95,27 @@ public class InfoActivity extends AppCompatActivity {
 		if (r2 > 0) {
 			    statusBarHeight = getResources().getDimensionPixelSize(r2);
 		}
-		_SetMargins(_toolbar, 0, statusBarHeight, 0, 0);
-		_SetMargins(Scroller, 0, 0, 0, navigationBarHeight);
-		VersionNameDetails.setText(versionName);
-		VersionCodeDetails.setText(versionCode);
-		VersionLinear.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View _view) {
-				
-			}
-		});
-		ProductionPhaseLinear.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View _view) {
-				
-			}
-		});
-		VersionCodeLinear.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View _view) {
-				
-			}
-		});
-		XEngineContainer.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View _view) {
-				
-			}
-		});
-		XEngineVersionLinear.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View _view) {
-				
-			}
-		});
-		User1.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View _view) {
-				UserViewIntent.setAction(Intent.ACTION_VIEW);
-				UserViewIntent.setData(Uri.parse("https://t.me/paxsenix0"));
-				startActivity(UserViewIntent);
-			}
-		});
-		User2.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View _view) {
-				UserViewIntent.setAction(Intent.ACTION_VIEW);
-				UserViewIntent.setData(Uri.parse("https://t.me/NEOAPPS_MUSIC"));
-				startActivity(UserViewIntent);
-			}
-		});
-		User3.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View _view) {
-				UserViewIntent.setAction(Intent.ACTION_VIEW);
-				UserViewIntent.setData(Uri.parse("https://t.me/PrakharDoneria"));
-				startActivity(UserViewIntent);
-			}
-		});
-		User4.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View _view) {
-				UserViewIntent.setAction(Intent.ACTION_VIEW);
-				UserViewIntent.setData(Uri.parse("https://t.me/trindadedev"));
-				startActivity(UserViewIntent);
-			}
-		});
-	}
-	
-	public void _CommandBlocks() {
+		_SetMargins(binding.Toolbar, 0, statusBarHeight, 0, 0);
+		_SetMargins(binding.Scroller, 0, 0, 0, navigationBarHeight);
+        
+        binding.linearTelegram.setOnClickListener(v -> openLink("https://t.me/xsignercommunity"));
+        binding.linearGithub.setOnClickListener(v -> openLink("https://github.com/Yamenher/X-Signer"));
+        binding.linearSupport.setOnClickListener(v -> sendMail());
+		
 	}
 	
 	
 	public void _SetMargins(final View _layout, final int _leftMargin, final int _TopMargin, final int _RightMargin, final int _BottomMargin) {
 		ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams) _layout.getLayoutParams();
-		
-		   layoutParams.setMargins(_leftMargin, _TopMargin, _RightMargin, _BottomMargin);
+		layoutParams.setMargins(_leftMargin, _TopMargin, _RightMargin, _BottomMargin);
 		_layout.setLayoutParams(layoutParams);
 	}
 	
 	
 	public double _DpToPx(final double _dp) {
 		  Resources resources = this.getResources();
-		    DisplayMetrics metrics = resources.getDisplayMetrics();
-		    return (double)Math.round(_dp * (metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT));
+		  DisplayMetrics metrics = resources.getDisplayMetrics();
+		  return (double)Math.round(_dp * (metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT));
 	}
 	
 	
@@ -349,10 +132,26 @@ public class InfoActivity extends AppCompatActivity {
 	
 	
 	public void _MakeTransition() {
-		Intent intent = new Intent(this, UpdateActivity.class);
-		intent.putExtra("version", VersionNameDetails.getText().toString());
-		ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(
-		    this, UpdateLinear, "transition2");
-		startActivity(intent, options.toBundle());
+		/*Intent intent = new Intent(this, UpdateActivity.class);
+		intent.putExtra("version", versionName);
+		ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(this, UpdateLinear, "transition2");
+		startActivity(intent, options.toBundle());*/
 	}
+    
+    public void openLink(String link) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(link));
+        startActivity(intent);
+    }
+    
+    public void sendMail() {
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+        emailIntent.setData(Uri.parse("mailto:xapps.feedback@gmail.com"));
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Feedback on X-Signer");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Hello, I wanted to share my feedback regarding X-Signer...");
+        try {
+            startActivity(emailIntent);
+        } catch (Exception e){
+            XUtil.showMessage(getApplicationContext(),"No mailing app was found");
+        }
+    }
 }
